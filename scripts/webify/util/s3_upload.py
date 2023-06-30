0095d72baf4b718366488aa173cce00ba06e1af6
Faris Chebib <faris@theluckybead.com>
2023-06-30 15:14:53 -0600

    Add webify ci yaml

--- scripts/webify/util/s3_upload.py
+++
@@ -1,5 +0,0 @@
-async def upload_to_s3(bucket, file_path, s3_path):
-    session = aioboto3.Session()
-    async with session.client('s3') as s3_client:
-        await s3_client.upload_file(file_path, bucket, s3_path)
-
